dfadbbcd68271399c883c4d30a088f7b5d86c527
arvention <amc.antioquia@yahoo.com.ph>
2018-08-15 23:59:04 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+
+class FasterRCNN(nn.Module):
+
+    """Faster R-CNN Architecture"""
+
+    def __init__(self, channels, class_count):
+        super(FasterRCNN, self).__init__()
+
+    def get_network(self):
+        pass  # TODO
+
+    def forward(self, x):
+        pass  # TODO
