d8144d67d4d003b9e034be7206c91d6817f906c3
arvention <amc.antioquia@yahoo.com.ph>
2018-08-17 19:48:27 +0800

    finished class BasicBlock

--- model.py
+++ model.py
@@ -1,15 +1,116 @@
 import torch.nn as nn
 
 
-class FasterRCNN(nn.Module):
+class Conv3x3_BN(nn.Module):
 
-    """Faster R-CNN Architecture"""
+    def __init__(self, in_channels, out_channels, stride):
+        super()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
 
-    def __init__(self, channels, class_count):
-        super(FasterRCNN, self).__init__()
+        self.net = self.get_network()
 
     def get_network(self):
-        pass  # TODO
+        layers = []
+        layers.append(nn.Conv2d(in_channels=self.in_channels,
+                                out_channels=self.out_channels,
+                                kernel_size=3,
+                                stride=self.stride,
+                                padding=1,
+                                bias=False))
+        layers.append(nn.BatchNorm2d(num_features=self.out_channels))
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        pass  # TODO
+        return self.net(x)
+
+
+class Block(nn.Module):
+    """
+    Block
+    """
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.downsample = downsample
+
+
+class BasicBlock(Block):
+    """
+    Basic residual block
+    """
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super().__init__(in_channels, out_channels, stride, downsample)
+
+        self.conv1 = self.get_conv1()
+        self.conv2 = self.get_conv2()
+        self.relu = nn.ReLU(inplace=True)
+
+    def get_conv1(self):
+        layers = []
+
+        layers.append(Conv3x3_BN(in_channels=self.in_channels,
+                                 out_channels=self.out_channels,
+                                 stride=self.stride))
+        layers.append(nn.ReLU(inplace=True))
+
+        return nn.Sequential(*layers)
+
+    def get_conv2(self):
+        layers = []
+
+        layers.append(Conv3x3_BN(in_channels=self.out_channels,
+                                 out_channels=self.out_channels,
+                                 stride=self.stride))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        y = self.conv1(x)
+        y = self.conv2(y)
+
+        if self.downsample is not None:
+            x = self.downsample(x)
+
+        y += x
+        y = self.relu(y)
+
+        return y
+
+
+class ResNet(nn.Module):
+
+    """ResNet Architecture"""
+
+    def __init__(self, channels, class_count):
+        super(ResNet, self).__init__()
+        self.channels = channels
+        self.class_count = class_count
+
+        self.net = self.get_network()
+
+    def get_network(self):
+        """
+        returns the structure of the network
+        """
+        layers = []
+        in_channels = self.channels
+
+        # conv1
+        layers.append(nn.Conv2d(in_channels=in_channels,
+                                out_channels=64,
+                                kernel_size=7,
+                                stride=2,
+                                padding=3,
+                                bias=False))
+        layers.append(nn.BatchNorm2d(num_features=64))
+        layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
+    def forward(self, x):
+        pass  # TODO:
